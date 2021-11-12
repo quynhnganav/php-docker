b2d459f5fec77fc3e97ea8e07e5fc36e94f1f7f3
quynhnganav <quynhnganav.com>
2021-11-12 10:18:11 +0700

    first commit

---
+++ app/index.php
@@ -0,0 +1,24 @@
+<?php 
+if(isset($_POST[''])) {
+
+}
+print_r($_GET)
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<meta charset="UTF-8">
+</head>
+<body>
+    $_POST
+    <form action="form.php" method="post">
+        <input type="text" placeholder='Enter name'>
+        <input type="password" placeholder="Enter password"><br>
+        <input type="submit" name='submit'>
+        
+    </form>
+</body>
+</html>
